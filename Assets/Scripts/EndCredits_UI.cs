fe630d6ec348e1cae8816e293386cc6038049715
IcedCaramelCoffee7 <bretyorkuire@gmail.com>
2025-11-16 14:44:55 -0500

    End Credits Scene
    Added End Credits Scene with buttons for functionality

---
+++ Assets/Scripts/EndCredits_UI.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndCredits_UI : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void ExitGame()
+    {
+        //Exit in build application
+        Application.Quit();
+
+        //Exit in Unity Editor (UNITY_EDITOR is not defined in a built game)
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #endif
+    }
+
+    public void ReturnToTitleScreen()
+    {
+        SceneManager.LoadScene("TitleScreen");
+    }
+}
