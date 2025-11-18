130e9df78d14b097b029d966c9124c9eacb2b107
IcedCaramelCoffee7 <bretyorkuire@gmail.com>
2025-11-17 21:57:17 -0500

    UI work

--- Assets/Scripts/InGame_UI.cs
+++ Assets/Scripts/InGame_UI.cs
@@ -4,6 +4,8 @@ using UnityEngine.UI;
 
 public class InGame_UI : MonoBehaviour
 {
+    [SerializeField] private GameObject pauseMenuUI;
+    [SerializeField] private CanvasGroup inGameUICanvasGroup;
     [SerializeField] private TextMeshProUGUI timerValue;
 
     private void Awake()
@@ -16,5 +18,23 @@ public class InGame_UI : MonoBehaviour
         timerValue.text = Time.timeSinceLevelLoad.ToString("F1") + "s";
     }
 
+    public void EnablePauseMenuUI()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0; // Freeze gameplay
 
+        // Disable interaction with gameplay UI while in pause menu
+        inGameUICanvasGroup.interactable = false;
+        inGameUICanvasGroup.blocksRaycasts = false;
+    }
+
+    public void DisablePauseMenuUI()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1; // Resume gameplay
+
+        // Re-enable interaction with gameplay UI when leaving pause menu
+        inGameUICanvasGroup.interactable = true;
+        inGameUICanvasGroup.blocksRaycasts = true;
+    }
 }
